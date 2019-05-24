83c38bc38d514d9647345935fdc644cea9cdb03b
Dennis Hagemeier <dennis.hagemeier@gmail.com>
2019-05-24 20:27:14 +0200

    Added "prefersDarkMode" detection

--- src/assets/js/theme-switch.js
+++ src/assets/js/theme-switch.js
@@ -1,5 +1,11 @@
+/*
+* Based on https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+* Modified with "prefers-color-scheme"
+*/
+
 const toggleSwitch = document.getElementById("theme-switch-input");
 const currentTheme = localStorage.getItem('theme');
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
@@ -9,13 +15,20 @@ if (currentTheme) {
     }
 }
 
+if (prefersDarkMode) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    toggleSwitch.checked = true;
+}
+
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }    
 }
 
