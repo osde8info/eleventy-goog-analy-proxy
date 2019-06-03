db32b346fa9637502718fcdeb40a92ca5e4d5293
Dennis Hagemeier <hello@d-hagemeier.de>
2019-06-03 16:22:07 +0200

    Changed twitter to direct api access

---
+++ src/_data/tweets.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var Twitter = require('twitter');
+require('dotenv').config()
+
+// https://developer.twitter.com/en/apps
+// https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+var params = {user_id: '550124146', count: 10};
+
+module.exports = async function() {
+  return client.get('statuses/user_timeline', params)
+    .catch((err) => {
+      console.error(err);
+    });
+}
