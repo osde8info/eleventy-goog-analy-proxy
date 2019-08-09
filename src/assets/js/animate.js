46607b29374ec62fcf8857c2912560eb9b88bb05
Dennis Hagemeier <hello@d-hagemeier.de>
2019-08-09 10:26:49 +0200

    Delay navigation only on desktop

--- src/assets/js/animate.js
+++ src/assets/js/animate.js
@@ -1,18 +1,22 @@
-function ready(e) {
-    (document.attachEvent ? "complete" === document.readyState : "loading" !== document.readyState) ? e(): document.addEventListener("DOMContentLoaded", e)
+function animate(screenwidth) {
+	if (screenwidth.matches) {
+		document.body.classList.add("loaded")
+	} else {
+		var navMainA = document.querySelectorAll("a");
+		document.addEventListener("click", function(e) {
+			if ((e.target.matches("a") || e.target.parentNode.matches("a")) && "_blank" !== e.target.getAttribute("target")) {
+				e.preventDefault();
+				var t = null !== e.target.getAttribute("href") ? e.target.getAttribute("href") : e.target.parentNode.getAttribute("href");
+				document.body.classList.remove("loaded"), setTimeout(function() {
+					window.location = t
+				}, 500)
+			}
+		}, !1);
+	}
 }
+var screenwidth = window.matchMedia("(max-width: 63.999em)")
+animate(screenwidth)
 
 window.onload = function () {
     document.body.classList.add("loaded")
 }
-
-var navMainA = document.querySelectorAll("a");
-document.addEventListener("click", function(e) {
-    if ((e.target.matches("a") || e.target.parentNode.matches("a")) && "_blank" !== e.target.getAttribute("target")) {
-        e.preventDefault();
-        var t = null !== e.target.getAttribute("href") ? e.target.getAttribute("href") : e.target.parentNode.getAttribute("href");
-        document.body.classList.remove("loaded"), setTimeout(function() {
-            window.location = t
-        }, 500)
-    }
-}, !1);
