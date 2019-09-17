08278b67c82c44a0642d417d38aa41bc1f6757e4
Dennis Hagemeier <hello@d-hagemeier.de>
2019-09-17 15:03:58 +0200

    Excluded links with hashes

--- src/assets/js/animate.js
+++ src/assets/js/animate.js
@@ -2,14 +2,15 @@ function animate(screenwidth) {
 	if (screenwidth.matches) {
 		document.body.classList.add("loaded")
 	} else {
-		var navMainA = document.querySelectorAll("a");
 		document.addEventListener("click", function(e) {
 			if ((e.target.matches("a") || e.target.parentNode.matches("a")) && "_blank" !== e.target.getAttribute("target")) {
-				e.preventDefault();
-				var t = null !== e.target.getAttribute("href") ? e.target.getAttribute("href") : e.target.parentNode.getAttribute("href");
-				document.body.classList.remove("loaded"), setTimeout(function() {
-					window.location = t
-				}, 500)
+				if(!e.target.hash) {
+					e.preventDefault();
+					var t = null !== e.target.getAttribute("href") ? e.target.getAttribute("href") : e.target.parentNode.getAttribute("href");
+					document.body.classList.remove("loaded"), setTimeout(function() {
+						window.location = t
+					}, 500)
+				}
 			}
 		}, !1);
 	}
