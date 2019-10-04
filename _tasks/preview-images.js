2e71270772a11c918cd1d4dc16a6d7614540f5db
Dennis Hagemeier <hello@d-hagemeier.de>
2019-10-04 13:38:25 +0200

    Skip already generated preview images

--- _tasks/preview-images.js
+++ _tasks/preview-images.js
@@ -4,12 +4,11 @@ const rawpreviewdata = fs.readFileSync('_site/preview-images.json');
 const previewdata = JSON.parse(rawpreviewdata);
 const dist = __dirname + "/../_site/assets/preview-images/";
 
-async function getscreen(filename) {
+async function getscreen(filename, url) {
   try {
     console.log("Getting " + filename);
-    let localurl = "file://" + dist + filename + ".html";
     await page.setViewport({ width: 1200, height: 628 });
-    await page.goto(localurl, { waitUntil: 'networkidle2' });
+    await page.goto(url, { waitUntil: 'networkidle2' });
     await page.screenshot({ 
       path: dist + filename + ".png",
       type: 'png'
@@ -23,16 +22,27 @@ async function getscreen(filename) {
 
 async function setpuppeteer(response) {  
 
-  browser = await puppeteer.launch({ headless: true });
-  page = await browser.newPage();
-
-  console.log("Launched Puppeteer");
-
-  var i;
-  for (i = 0; i < response.length; i++) {
-    await getscreen(response[i].filename);
-  }
+  try {
+    browser = await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
   
+    var i;
+    for (i = 0; i < response.length; i++) {
+  
+      let filename = response[i].filename;
+      let localurl = dist + filename;
+  
+      if (fs.existsSync(localurl + ".png")) {
+        console.log("File exists");
+      } else {
+        await getscreen(filename, "file://" + localurl + ".html");
+      }
+  
+    }
+  } catch(err) {
+    console.error(err)
+  }
+
   browser.close();
   console.log('Done!');
 
