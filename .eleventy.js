fa06d0bd19a59d4e54b9bcbb9d2d6090638e8c77
Dennis Hagemeier <dennis.hagemeier@gmail.com>
2019-10-09 20:28:52 +0200

    Added basic webmention support

--- .eleventy.js
+++ .eleventy.js
@@ -37,6 +37,46 @@ module.exports = function(eleventyConfig) {
     var result = obj.replace(/# /g, "");
     return result;
   });
+  
+  // Webmention Filter: By URL
+  eleventyConfig.addFilter("webmentionsByUrl", function(webmentions, url) {
+    const allowedTypes = ['mention-of', 'in-reply-to']
+    const allowedHTML = {
+        allowedTags: ['b', 'i', 'em', 'strong', 'a'],
+        allowedAttributes: {
+            a: ['href']
+        }
+    }
+
+    const orderByDate = (a, b) =>
+        new Date(a.published) - new Date(b.published)
+
+    const checkRequiredFields = entry => {
+        const { author, published, content } = entry
+        return !!author && !!author.name && !!published && !!content
+    }
+
+    const clean = entry => {
+        const { html, text } = entry.content
+
+        if (html && html.length > 2000) {
+            // really long html mentions, usually newsletters or compilations
+            entry.content.value = `mentioned this in <a href="${entry.url}">${entry.url}</a>`
+        } else {
+            entry.content.value = html ? sanitizeHTML(html, allowedHTML) : sanitizeHTML(text, allowedHTML)
+        }
+
+        return entry
+    }
+
+    return webmentions
+        .filter(entry => entry['wm-target'] === url)
+        .filter(entry => allowedTypes.includes(entry['wm-property']))
+        .filter(checkRequiredFields)
+        .sort(orderByDate)
+        .map(clean)
+  });
+
   // Custom slug
   eleventyConfig.addFilter("pslug", obj => {
     var result = obj.trim().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=_'`~()]/g, '').replace(/\s+/g, '-');
---
+++ src/_data/webmentions.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+require('dotenv').config()
+
+module.exports = async function() {
+    const origin = 'https://webmention.io/api/mentions.jf2'
+    const token = process.env.WEBMENTION_TOKEN
+    const url = `${origin}?token=${token}`
+
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const feed = await response.json()
+            return feed
+        }
+    } catch (err) {
+        console.error(err)
+        return null
+    }
+}
