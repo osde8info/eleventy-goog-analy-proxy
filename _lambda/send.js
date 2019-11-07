1725c8d01b9a4c3afcfa2eac852d1a5eadd592bd
Dennis Hagemeier <hello@d-hagemeier.de>
2019-11-07 09:06:59 +0100

    Changed firestore to ga, deleted some dependencies

---
+++ _lambda/send.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+// https://ga-dev-tools.appspot.com/hit-builder/?v=1&t=pageview&tid=UA-85526167-1&cid=c1d6f00d-8b04-445c-9ea4-d7e8875044ec&ua=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F78.0.3904.87%20Safari%2F537.36&aip=1&ds=web&dl=https%3A%2F%2Fwww.d-hagemeier.com%2Fen%2Farticles%2Fembed-twitter%2F%3Futm_source%3Dnewsletter%26utm_medium%3Demail%26utm_campaign%3Dtestnewsletter&dt=Dies%20ist%20ein%20Titel&ul=en-US&dr=https%3A%2F%2Fwww.gmail.com%2F
+
+exports.handler = async (event, context) => {
+  const data = JSON.parse(event.body);
+  const endpoint = "https://www.google-analytics.com/collect";
+  const payload = encodeURI(`v=1&t=pageview&tid=UA-85526167-1&cid=${data.uuid}&ua=${data.useragent}&aip=1&ds=web&dl=${data.url}&dt=${data.title}&ul=${data.language}&dr=${data.referrer}`).replace(/\//g, '%2F');
+
+  try {
+      const response = await fetch(`${endpoint}?${payload}`, {
+        method: 'POST',
+        cache: 'no-cache'
+      })
+      if (response.ok) {
+          const feed = await response.json()
+          console.log(feed)
+      }
+  } catch (err) {
+      console.error("Error: " + err)
+  }
+
+};
--- src/assets/js/send.js
+++ src/assets/js/send.js
@@ -1,23 +1,22 @@
-const timestamp = Date.now();
+function uuidv4() { return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)); }
+const uuid = uuidv4();
 const useragent = navigator.userAgent;
 const title = document.title;
-const url = window.location.hostname + window.location.pathname;
-const parameter = window.location.search;
+const url = window.location.origin + window.location.pathname + window.location.search;
 const referrer = document.referrer;
 const language = navigator.language || navigator.userLanguage;
 
 const sitedata = {
-  timestamp: timestamp,
+  uuid: uuid,
   useragent: useragent,
   title: title,
   url: url,
-  parameter: parameter,
   referrer: referrer,
   language: language
 }
 
 function senddata(data) {
-  return fetch('/.netlify/functions/send/send', {
+  return fetch('/.netlify/functions/send', {
     body: JSON.stringify(data),
     method: 'POST'
   }).then(response => {
@@ -26,7 +25,7 @@ function senddata(data) {
 }
 
 senddata(sitedata).then((response) => {
-  console.log('API response', response)
+  console.log('Success: ', response)
 }).catch((error) => {
-  console.log('API error', error)
+  console.log('Error: ', error)
 })
