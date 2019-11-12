bc5e3461a0289fcf54af0901a19027e42cb8426c
Dennis Hagemeier <hello@d-hagemeier.de>
2019-11-12 09:35:37 +0100

    Security fixes

--- functions/send/send.js
+++ functions/send/send.js
@@ -1,12 +1,22 @@
 const fetch = require('node-fetch');
 require('dotenv').config()
 
-// https://ga-dev-tools.appspot.com/hit-builder/?v=1&t=pageview&tid=UA-85526167-1&cid=c1d6f00d-8b04-445c-9ea4-d7e8875044ec&ua=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F78.0.3904.87%20Safari%2F537.36&aip=1&ds=web&dl=https%3A%2F%2Fwww.d-hagemeier.com%2Fen%2Farticles%2Fembed-twitter%2F%3Futm_source%3Dnewsletter%26utm_medium%3Demail%26utm_campaign%3Dtestnewsletter&dt=Dies%20ist%20ein%20Titel&ul=en-US&dr=https%3A%2F%2Fwww.gmail.com%2F
-
 exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  
   const data = JSON.parse(event.body);
+  const whitelist = "d-hagemeier";
+  const origin = data.origin;
+  const url = origin + data.pathname + data.search;
+
+  if (!origin.includes(whitelist)) {
+    return { statusCode: 403, body: "Domain Not Whitelisted" };
+  }
+  
   const endpoint = "https://www.google-analytics.com/collect";
-  const payload = encodeURI(`v=1&t=pageview&tid=UA-85526167-1&cid=${data.uuid}&ua=${data.useragent}&aip=1&ds=web&dl=${data.url}&dt=${data.title}&ul=${data.language}&dr=${data.referrer}`).replace(/\//g, '%2F');
+  const payload = encodeURI(`v=1&t=pageview&tid=UA-85526167-1&cid=${data.uuid}&ua=${data.useragent}&aip=1&ds=web&dl=${url}&dt=${data.title}&ul=${data.language}&dr=${data.referrer}`).replace(/\//g, '%2F');
 
   try {
       const response = await fetch(`${endpoint}?${payload}`, {
--- src/assets/js/send.js
+++ src/assets/js/send.js
@@ -2,7 +2,9 @@ function uuidv4() { return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (
 const uuid = uuidv4();
 const useragent = navigator.userAgent;
 const title = document.title;
-const url = window.location.origin + window.location.pathname + window.location.search;
+const origin = window.location.origin;
+const pathname = window.location.pathname;
+const search = window.location.search;
 const referrer = document.referrer;
 const language = navigator.language || navigator.userLanguage;
 
@@ -10,7 +12,9 @@ const sitedata = {
   uuid: uuid,
   useragent: useragent,
   title: title,
-  url: url,
+  origin: origin,
+  pathname: pathname,
+  search: search,
   referrer: referrer,
   language: language
 }
@@ -19,8 +23,6 @@ function senddata(data) {
   return fetch('/.netlify/functions/send', {
     body: JSON.stringify(data),
     method: 'POST'
-  }).then(response => {
-    // console.log(response)
   })
 }
 
